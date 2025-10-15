d466b652ce35d23a9285565ccce6cf38d49e1ab5
Anand-kanti <163557780+Anand-kanti@users.noreply.github.com>
2025-10-16 00:11:24 +0530

    First commit

---
+++ java-practice/src/Java_8/DateAndTime.java
@@ -0,0 +1,10 @@
+package Java_8;
+
+import java.time.LocalDate;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current date: " + currentDate);
+    }
+}
