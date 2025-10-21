5b79b9a546382bb726f485947e86ee4a47330f03
Anand-kanti <163557780+Anand-kanti@users.noreply.github.com>
2025-10-21 17:27:06 +0530

    Palindrome.java

--- java-practice/src/Java_8/Palindrome.java
+++ java-practice/src/Java_8/Palindrome.java
@@ -1,1 +1,10 @@
+package Java_8;
 
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "mam";
+
+        boolean res = str.equalsIgnoreCase(new StringBuffer(str).reverse().toString());
+        System.out.println(res);
+    }
+}
