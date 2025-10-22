9e6e6126cb62a52a74fc8c1ce0010b63314ee1ca
Anand-kanti <163557780+Anand-kanti@users.noreply.github.com>
2025-10-23 00:32:23 +0530

    Array Programs

---
+++ java-practice/src/Java_8/Array/ReverseArrayInPlace.java
@@ -0,0 +1,27 @@
+package Java_8.Array;
+
+import java.util.Arrays;
+
+public class ReverseArrayInPlace {
+    public static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Original: " + Arrays.toString(arr));
+
+        reverse(arr);
+
+        System.out.println("Reversed: " + Arrays.toString(arr));
+    }
+}
